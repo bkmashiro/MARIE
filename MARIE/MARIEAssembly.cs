aa98f03a17e433c7dbcb7763424c738067c69907
bakamashiro <53376445+bkmashiro@users.noreply.github.com>
2023-04-04 16:26:53 +0800

    fix

--- MARIE/MARIEAssembly.cs
+++ MARIE/MARIEAssembly.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel.Design.Serialization;
 using static MARIE.MARIEAssembly;
 
 namespace MARIE
@@ -36,8 +37,9 @@ namespace MARIE
 
         //AC MAR MBR
 
-        class microInstruction
+        public class microInstruction
         {
+            IMicroCodeObject left, right;
             public microInstruction(string s)
             {
                 //if is assignment:  xx <- yy
@@ -46,21 +48,34 @@ namespace MARIE
                     string[] lr = s.Split("<-");
                     var l = lr[0];
                     var r = lr[1];
+                    left = MicroCodeObjectFactory.GetMicroCodeObjectByStr(l);
+                    right = MicroCodeObjectFactory.GetMicroCodeObjectByStr(r);
                 }
-
+            }
+            public override string ToString()
+            {
+                return $"{left}<-{right}";
             }
         }
-        static class MicroCodeObjectFactory
+        class MicroCodeObjectFactory
         {
             //M[MBR]
-            IMicroCodeObject GetMicroCodeObjectByStr(string s)
+            public static IMicroCodeObject GetMicroCodeObjectByStr(string s)
             {
+                //if is a number
+                if (int.TryParse(s, out int num))
+                {
+                    return new Registers.Immediate(num);
+                }
+
+
                 //if has params
                 if (s.Contains('['))
                 {
                     if (s[0]=='M') //is memory
                     {
-                        string param = s.Split('[');
+                        string param = s.Split('[', ']')[1];
+                        return new Registers.M(GetMicroCodeObjectByStr(param));
                     }
                 }
                 else
@@ -72,6 +87,7 @@ namespace MARIE
                         case "MBR":return new Registers.MBR();
                         case "IR":return new Registers.IR();
                         case "PC":return new Registers.PC();
+                        case "AC":return new Registers.AC();
                         default:
                             break;
                     }
@@ -82,19 +98,45 @@ namespace MARIE
 
         public interface IMicroCodeObject
         {
-            int GetValue() { return 0; }
-            void SetValue(Dictionary<string, int>? env = null) { }
+            public int GetValue();
+            public void SetValue(Dictionary<string, int>? env = null);
         }
 
-        class MCO : IMicroCodeObject
+        abstract class MCO : IMicroCodeObject
         {
             public MCO(string s)
             {
 
             }
-        }
 
-        class ASM
+            public int GetValue()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SetValue(Dictionary<string, int>? env = null)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class ASMFactory
+        {
+            public static ASM GetASM(string s)
+            {
+                string[] ss = s.Split(' ');
+                string ins = ss[0], param = ss[1];
+
+                switch (ins)
+                {
+                    case "LOAD": return new LOAD(param);
+                    default:
+                        break;
+                }
+
+                throw new InvalidDataException();
+            }
+        }
+        public class ASM
         {
             List<microInstruction> microInstructions = new();
             protected void init(params string[] s)
@@ -107,26 +149,44 @@ namespace MARIE
             }
         }
 
-        class JNS : ASM
+        class LOAD : ASM
         {
-            public JNS(int X)
+            public LOAD(string X)
             {
                 init(
-                    "MBR<-PC",
-                    "MAR<-X",
-                    "M[MBR]<-MBR",
-                    "MBR<-X",
-                    "AC<-1",
-                    "AC<-AC + MBR",
-                    "PC<-AC"
+                    $"MAR<-{X}",
+                    $"MBR<-M[MAR]",
+                    $"AC<-MBR"
+                );
+            }
+        }
+
+        class JNS : ASM
+        {
+            public JNS(string X)
+            {
+                init(
+                    $"MBR<-PC",
+                    $"MAR<-{X}",
+                    $"M[MBR]<-MBR",
+                    $"MBR<-{X}",
+                    $"AC<-1",
+                    $"AC<-AC+MBR",
+                    $"PC<-AC"
                 );
             }
 
         }
 
-        string assemble(string asmc)
+        public List<ASM> Assemble(string asmc)
         {
-            return "";
+            string[] asmc_lines = asmc.Split('\n');
+            List<ASM> microInstructions = new();
+            foreach (string asmc_line in asmc_lines)
+            {
+                microInstructions.Add(ASMFactory.GetASM(asmc));
+            }
+            return microInstructions;
         }
     }
 }
@@ -134,24 +194,170 @@ namespace MARIE
 
 namespace Registers
 {
+    class Immediate : IMicroCodeObject //立即数
+    {
+
+        int value = -1;
+        public Immediate(int value)
+        {
+            this.value = value;
+        }
+
+        public int GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"DEC{this.GetValue()}";
+        }
+    }
     class IR : IMicroCodeObject
     {
+        int value = -1;
+
+        public int GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"IR({this.GetValue()})";
+        }
+
 
     }
     class PC : IMicroCodeObject
     {
+        int value = -1;
 
+        public int GetValue()
+        {
+            return value;
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"PC({GetValue()})";
+        }
     }
     class MBR : IMicroCodeObject
     {
+        int value = -1;
 
+        public int GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"MBR({GetValue()})";
+        }
     }
     class MAR : IMicroCodeObject
     {
+        int value = -1;
 
+        public int GetValue()
+        {
+            return value;
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"MAR({GetValue()})";
+        }
     }
     class M : IMicroCodeObject//memory
     {
-        public M(string val) { }
+        private IMicroCodeObject microCodeObject;
+
+        public M(IMicroCodeObject proxy) {
+            this.microCodeObject = proxy;
+        }
+
+        int value = -1;
+        public override string ToString()
+        {
+            return $"IR({GetValue()})";
+        }
+
+        public int GetValue()
+        {
+            return this.value;
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class AC : IMicroCodeObject
+    {
+
+        int value = -1;
+
+        public int GetValue()
+        {
+            return value;
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"AC({GetValue()})";
+        }
+    }
+    class Compositonal : IMicroCodeObject
+    {
+        int value = -1;
+
+        public int GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(Dictionary<string, int>? env = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"IR({value})";
+        }
     }
 }
--- MARIE/Program.cs
+++ MARIE/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using MARIE;
+
+MARIEAssembly MARIE = new();
+
+var v =MARIE.Assemble("LOAD 1");
+
+Console.WriteLine(v);
